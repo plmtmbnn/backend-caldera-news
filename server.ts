fe27c5628b60649cf99e9ed42c87717db8a7572b
polmatambunan <polma.tambunan@fit.id>
2022-07-02 13:35:41 +0700

    change server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from './App';
 
 const PORT = process.env.BE_PORT || 8080;
+const host = '0.0.0.0';
 
-app.listen(PORT, () => {
+app.listen(PORT, host, () => {
   console.log(`Backend Caldera is running on port: ${PORT}`);
 });
