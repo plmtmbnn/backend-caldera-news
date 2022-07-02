f35e4cfd1ba4cf46cbc2371a8e4532372763b3f3
polmatambunan <polma.tambunan@fit.id>
2022-07-02 15:47:47 +0700

    change env

--- sequelize/init.ts
+++ sequelize/init.ts
@@ -1,19 +1,12 @@
 import { Sequelize } from 'sequelize';
-import CryptoJS from 'crypto-js';
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
-  CryptoJS.AES.decrypt(process.env.DB_USER.toString(), process.env.SECRET_KEY.toString()).toString(CryptoJS.enc.Utf8),
-  CryptoJS.AES.decrypt(process.env.DB_PASS.toString(), process.env.SECRET_KEY.toString()).toString(CryptoJS.enc.Utf8),
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT),
-    pool: {
-      max: 30,
-      min: 0,
-      acquire: 60000,
-      idle: 10000
-    },
     logging: false,
     dialect: 'postgres',
     dialectOptions: {
