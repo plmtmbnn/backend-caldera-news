d5e7238c30e1a0cdd629e42447f7fab7d20e69fb
polmatambunan <polma.tambunan@fit.id>
2022-10-30 11:58:01 +0700

    ad

--- service/NewsService.ts
+++ service/NewsService.ts
@@ -118,7 +118,7 @@ export class NewsService {
               transaction
             });
             let insertTag: any[] = [];
-            for (const iterator of Array(...tag_ids)) {
+            for (const iterator of tag_ids) {
               console.log('iterator', iterator);
               
               insertTag.push(
