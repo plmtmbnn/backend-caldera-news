7ed332bf995b8e741138d30ffda4a82aaa5b454e
polmatambunan <polma.tambunan@fit.id>
2022-12-08 23:28:26 +0700

    fiux

--- controller/CategoryController.ts
+++ controller/CategoryController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { CategoryService } from '../service/CategoryService';
 import { ResponseHandler } from '../helper/ResponseHandler';
 import path from 'path';
+import fs from 'fs';
 
 export class CategoryController {
   async getCategory (req: Request, res: Response): Promise<void> {
@@ -28,7 +29,8 @@ export class CategoryController {
       const file_name: string = path.join(
         __dirname, '..', '..', '/metatag', `${req.params.news_url}.json`);
 
-      const metatag = require(`./${file_name}`);
+      const metatag = require(`${file_name}`);
+
       const message: string = metatag?.title || 'caldera.id' ;
       res.write(message);
       res.end();
@@ -44,7 +46,7 @@ export class CategoryController {
       const file_name: string = path.join(
         __dirname, '..', '..', '/metatag', `${req.params.news_url}.json`);
 
-      const metatag = require(`./${file_name}`);
+      const metatag = require(`${file_name}`);
       const message: string = metatag?.image_url || 'https://www.caldera.id/static/media/logo.f22abae4ca373ed8a7b7.png' ;
       res.write(message);
       res.end();
