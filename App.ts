330a84ef519fac1c97448206ef04358bc35e2617
polmatambunan <polma.tambunan@fit.id>
2022-12-11 18:58:53 +0700

    fix

--- App.ts
+++ App.ts
@@ -26,13 +26,7 @@ class App {
       this.app.use(express.json({ limit: '100mb' }));
       this.app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
-      this.app.use(express.static(path.join(__dirname, "build")));
-
-      this.app.use('/auth', authRoute);
-      this.app.use('/news', newsRoute);
-      this.app.use('/category', categoryRoute);
-      
-      this.app.use('/', router);
+      this.app.use(express.static(path.join(__dirname, '..', '..', '/frontend')));
 
       this.app.get("*", (req: Request, res: Response) => {
         console.log('Trying to request', req);
@@ -40,6 +34,12 @@ class App {
         res.sendFile(path.join(__dirname, '..', '..', '/frontend', "index.html"))
         }
       );
+
+      this.app.use('/auth', authRoute);
+      this.app.use('/news', newsRoute);
+      this.app.use('/category', categoryRoute);
+      
+      this.app.use('/', router);
 
       var avatarDir = './image/avatar';
       var newsDir = './image/news';
