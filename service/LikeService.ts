f1a6e294724d285bce7c52490cb9cce58e8c3777
polmatambunan <polma.tambunan@fit.id>
2023-03-30 17:10:19 +0700

    i

--- service/LikeService.ts
+++ service/LikeService.ts
@@ -64,7 +64,7 @@ export class LikeService {
       }
 
       const binary = fs.readFileSync(current_path);
-      res.writeHead(200, { 'Content-Type': 'image/jpeg', 'Content-Encoding': 'gzip', 'Transfer-Encoding': 'chunked' });
+      res.writeHead(200, { 'Content-Type': 'image/jpeg', 'Content-Encoding': 'gzip' });
       res.end(binary, 'binary');
       
     } catch (error) {
